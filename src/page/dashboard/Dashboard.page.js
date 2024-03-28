720abb6b6f1312010ea45e051a90e4ebc63838d2
Mayukh Maity <maitymayukh2003@gmail.com>
2024-03-28 19:51:44 +0530

    dashboard page added

--- src/page/dashboard/Dashboard.page.js
+++ src/page/dashboard/Dashboard.page.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import {Container, Row, Col, Button} from 'react-bootstrap';
+import { TicketTable } from '../../components/ticket-table/TicketTable.comp';
+import tickets from '../../assets/data/dummy-tickets.json';
 
 export const Dashboard = () => {
     return (
         <Container>
             <Row>
-                <Col>
-                    <Button>Add New Ticket</Button>
+                <Col className='text-center mt-5 mb-2'>
+                    <Button 
+                    variant='info' 
+                    style={{'font-size':'2rem', padding:"10px 30px" }}>
+                    Add New Ticket
+                    </Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col className='mt-2'>
+                    Recently Added Ticekts
+                </Col>
+            </Row>
+
+            <hr />
+
+            <Row>
+                <Col className='return-ticket'>
+                    <TicketTable tickets={tickets}/>
                 </Col>
             </Row>
         </Container>
